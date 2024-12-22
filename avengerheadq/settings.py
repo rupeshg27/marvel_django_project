5d698a0d78d80a538cd1a0fab426a192906db238
rupeshg27 <rupesh19605@gmail.com>
2024-12-22 22:57:34 +0530

    Fix: Add STATIC_ROOT for static files

--- avengerheadq/settings.py
+++ avengerheadq/settings.py
@@ -128,6 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/accounts/login'
 
